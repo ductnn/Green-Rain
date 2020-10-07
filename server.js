06707fdcaa674a840dfe30bbab0299255cd89ae8
DuctnNetwork <trannhuduc531998@gmail.com>
2020-10-07 21:57:45 +0700

    Fix env port starting

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || PORT_START;
 
 const app = new express();
 
